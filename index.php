9b34938a324fb61632ceb930a54fdbaa96e546ef
Yotman Reyes <32025446+yotman99@users.noreply.github.com>
2020-10-11 10:29:09 -0400

    fix index.php autoload include error

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-    require_once "../vendor/autoload.php";
+    require_once __DIR__ . "/vendor/autoload.php";
 
     try {
         $bot = new \TelegramBot\Api\Client('1239904902:AAHWGS40MoO1x2wcvPs-SuzMmO_R-X28EOw', '571e37d4-a0b4-439f-a677-5f9ca6956803');
